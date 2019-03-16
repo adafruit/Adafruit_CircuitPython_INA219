ee35938aafd597d931eec45f776fb2347d5a0cbb
Jean-Michel Mercier <jean-michel@chez-mercier.net>
2019-03-16 21:33:11 +0100

    Update sample to use new properties and clean-up printed output (align values)

--- examples/ina219_simpletest.py
+++ examples/ina219_simpletest.py
@@ -9,13 +9,22 @@ i2c_bus = busio.I2C(SCL, SDA)
 
 ina219 = adafruit_ina219.INA219(i2c_bus)
 
+# change configuration to use 32 samples averaging for both bus voltage and shunt voltage
+ina219.bus_adc_res = 0x0D       # BADCRES_12BIT_32S_17MS
+ina219.shunt_adc_res = 0x0D     # SADCRES_12BIT_32S_17MS
+ina219.bus_voltage_range = 0    # BVOLTAGERANGE_16V
+
 print("ina219 test")
 
 while True:
-    print("Bus Voltage:   {} V".format(ina219.bus_voltage))
-    print("Shunt Voltage: {} mV".format(ina219.shunt_voltage / 1000))
-    print("Load Voltage:  {} V".format(ina219.bus_voltage + ina219.shunt_voltage))
-    print("Current:       {} mA".format(ina219.current))
+    bus_voltage = ina219.bus_voltage
+    shunt_voltage = ina219.shunt_voltage
+    current = ina219.current
+
+    print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
+    print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
+    print("Load Voltage:  {:6.3f} V".format(bus_voltage))
+    print("Current:       {:9.6f} A".format(current/1000))
     print("")
 
     time.sleep(2)
