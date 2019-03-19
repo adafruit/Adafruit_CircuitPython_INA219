3056c2ca442d8559d152a3f9951fec550052656c
Barbudor <barbudor@barbudor.net>
2019-03-19 21:00:55 +0100

    removed un-necessary `const()` keyword

--- adafruit_ina219.py
+++ adafruit_ina219.py
@@ -62,40 +62,40 @@ _REG_CONFIG                 = const(0x00)
 
 class BusVoltageRange:
     """Constants for ``bus_voltage_range``"""
-    RANGE_16V               = const(0x00)       # set bus voltage range to 16V
-    RANGE_32V               = const(0x01)       # set bus voltage range to 32V (default)
+    RANGE_16V               = 0x00      # set bus voltage range to 16V
+    RANGE_32V               = 0x01      # set bus voltage range to 32V (default)
 
 class Gain:
     """Constants for ``gain``"""
-    DIV_1_40MV              = const(0x00)       # shunt prog. gain set to  1, 40 mV range
-    DIV_2_80MV              = const(0x01)       # shunt prog. gain set to /2, 80 mV range
-    DIV_4_160MV             = const(0x02)       # shunt prog. gain set to /4, 160 mV range
-    DIV_8_320MV             = const(0x03)       # shunt prog. gain set to /8, 320 mV range
+    DIV_1_40MV              = 0x00      # shunt prog. gain set to  1, 40 mV range
+    DIV_2_80MV              = 0x01      # shunt prog. gain set to /2, 80 mV range
+    DIV_4_160MV             = 0x02      # shunt prog. gain set to /4, 160 mV range
+    DIV_8_320MV             = 0x03      # shunt prog. gain set to /8, 320 mV range
 
 class ADCResolution:
     """Constants for ``bus_adc_resolution`` or ``shunt_adc_resolution``"""
-    ADCRES_9BIT_1S          = const(0x00)       #  9bit,   1 sample,     84us
-    ADCRES_10BIT_1S         = const(0x01)       # 10bit,   1 sample,    148us
-    ADCRES_11BIT_1S         = const(0x02)       # 11 bit,  1 sample,    276us
-    ADCRES_12BIT_1S         = const(0x03)       # 12 bit,  1 sample,    532us
-    ADCRES_12BIT_2S         = const(0x09)       # 12 bit,  2 samples,  1.06ms
-    ADCRES_12BIT_4S         = const(0x0A)       # 12 bit,  4 samples,  2.13ms
-    ADCRES_12BIT_8S         = const(0x0B)       # 12bit,   8 samples,  4.26ms
-    ADCRES_12BIT_16S        = const(0x0C)       # 12bit,  16 samples,  8.51ms
-    ADCRES_12BIT_32S        = const(0x0D)       # 12bit,  32 samples, 17.02ms
-    ADCRES_12BIT_64S        = const(0x0E)       # 12bit,  64 samples, 34.05ms
-    ADCRES_12BIT_128S       = const(0x0F)       # 12bit, 128 samples, 68.10ms
+    ADCRES_9BIT_1S          = 0x00      #  9bit,   1 sample,     84us
+    ADCRES_10BIT_1S         = 0x01      # 10bit,   1 sample,    148us
+    ADCRES_11BIT_1S         = 0x02      # 11 bit,  1 sample,    276us
+    ADCRES_12BIT_1S         = 0x03      # 12 bit,  1 sample,    532us
+    ADCRES_12BIT_2S         = 0x09      # 12 bit,  2 samples,  1.06ms
+    ADCRES_12BIT_4S         = 0x0A      # 12 bit,  4 samples,  2.13ms
+    ADCRES_12BIT_8S         = 0x0B      # 12bit,   8 samples,  4.26ms
+    ADCRES_12BIT_16S        = 0x0C      # 12bit,  16 samples,  8.51ms
+    ADCRES_12BIT_32S        = 0x0D      # 12bit,  32 samples, 17.02ms
+    ADCRES_12BIT_64S        = 0x0E      # 12bit,  64 samples, 34.05ms
+    ADCRES_12BIT_128S       = 0x0F      # 12bit, 128 samples, 68.10ms
 
 class Mode:
     """Constants for ``mode``"""
-    POWERDOW                = const(0x00)       # power down
-    SVOLT_TRIGGERED         = const(0x01)       # shunt voltage triggered
-    BVOLT_TRIGGERED         = const(0x02)       # bus voltage triggered
-    SANDBVOLT_TRIGGERED     = const(0x03)       # shunt and bus voltage triggered
-    ADCOFF                  = const(0x04)       # ADC off
-    SVOLT_CONTINUOUS        = const(0x05)       # shunt voltage continuous
-    BVOLT_CONTINUOUS        = const(0x06)       # bus voltage continuous
-    SANDBVOLT_CONTINUOUS    = const(0x07)       # shunt and bus voltage continuous
+    POWERDOW                = 0x00      # power down
+    SVOLT_TRIGGERED         = 0x01      # shunt voltage triggered
+    BVOLT_TRIGGERED         = 0x02      # bus voltage triggered
+    SANDBVOLT_TRIGGERED     = 0x03      # shunt and bus voltage triggered
+    ADCOFF                  = 0x04      # ADC off
+    SVOLT_CONTINUOUS        = 0x05      # shunt voltage continuous
+    BVOLT_CONTINUOUS        = 0x06      # bus voltage continuous
+    SANDBVOLT_CONTINUOUS    = 0x07      # shunt and bus voltage continuous
 
 # SHUNT VOLTAGE REGISTER (R)
 _REG_SHUNTVOLTAGE           = const(0x01)
