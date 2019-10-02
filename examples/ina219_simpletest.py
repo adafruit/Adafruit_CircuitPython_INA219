d1e68beb5773beb2e79958bf4d8700c1ae9727e9
wallarug <wallarug@coliemore.com.au>
2019-10-02 22:00:17 +1000

    Restored Original Test

--- examples/ina219_simpletest.py
+++ examples/ina219_simpletest.py
@@ -7,8 +7,7 @@ from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
 i2c_bus = board.I2C()
 
-ina219 = INA219(i2c_bus, 0x41)
-ina219.set_calibration_16V_5A()
+ina219 = INA219(i2c_bus)
 
 print("ina219 test")
 
@@ -22,24 +21,22 @@ print("  mode:                 0x%1X" % ina219.mode)
 print("")
 
 # optional : change configuration to use 32 samples averaging for both bus voltage and shunt voltage
-#ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-#ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 # optional : change voltage range to 16V
-#ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
+ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
 
 # measure and display loop
 while True:
     bus_voltage = ina219.bus_voltage        # voltage on V- (load side)
     shunt_voltage = ina219.shunt_voltage    # voltage between V+ and V- across the shunt
     current = ina219.current                # current in mA
-    power = ina219.power		    # power in watts
 
     # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
     print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
     print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
     print("Load Voltage:  {:6.3f} V".format(bus_voltage))
     print("Current:       {:9.6f} A".format(current/1000))
-    print("Power:         {:9.6f} mW".format(power*1000))
     print("")
 
     time.sleep(2)
