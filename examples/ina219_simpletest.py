50683c9b319775cd59597f8f7850871ddde81049
dherrada <dylan.herrada@gmail.com>
2020-03-16 16:34:57 -0400

    Ran black, updated to pylint 2.x

--- examples/ina219_simpletest.py
+++ examples/ina219_simpletest.py
@@ -28,15 +28,15 @@ ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
 
 # measure and display loop
 while True:
-    bus_voltage = ina219.bus_voltage        # voltage on V- (load side)
-    shunt_voltage = ina219.shunt_voltage    # voltage between V+ and V- across the shunt
-    current = ina219.current                # current in mA
+    bus_voltage = ina219.bus_voltage  # voltage on V- (load side)
+    shunt_voltage = ina219.shunt_voltage  # voltage between V+ and V- across the shunt
+    current = ina219.current  # current in mA
 
     # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
     print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
     print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
     print("Load Voltage:  {:6.3f} V".format(bus_voltage))
-    print("Current:       {:9.6f} A".format(current/1000))
+    print("Current:       {:9.6f} A".format(current / 1000))
     print("")
 
     time.sleep(2)
